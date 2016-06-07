9a316cb74c15476ec3b9253b827ca1403cd78f8c
dudasc <dudatimbe@gmail.com>
2016-06-06 22:25:30 -0300

    9

--- app/Controller/ContatosController.php
+++ app/Controller/ContatosController.php
@@ -12,8 +12,9 @@ class ContatosController extends AppController {
 			$name = $this->data['Contato']['nome'];
 			$from = $this->data['Contato']['email'];
 			$subject = 'Contato do Site limamoveissobmedida.com.br';
+			$cidade = $this->data['Contato']['cidade'];
 			$fone = $this->data['Contato']['telefone'];
-			$msg = 'Cliente:'.$name.'<br>Telefone: '.$fone . "<br><br>" . $this->data['Contato']['msg'];
+			$msg = 'Cliente:'.$name.'<br>Cidade: '.$cidade.'<br>Telefone: '.$fone . "<br><br>" . $this->data['Contato']['mensagem'];
 		
 			$this->Email->sendAs = 'both';
 			// html, text, both
@@ -21,7 +22,7 @@ class ContatosController extends AppController {
 		
 			$this->set('from', $name);
 			$this->set('msg', $msg);
-			$this->Email->to = 'contato@limamoveissobmedida.com.br';
+			$this->Email->to = 'contato@lenergy.com.br';
 			//$this -> Email -> to = 'eduardoscoelho@hotmail.com';
 			$this->Email->subject = $subject;
 			$this->Email->replyTo = $from;
